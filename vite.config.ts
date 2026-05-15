6ef6e66eab2607100cd90f8214ad4c37500b07cd
LeonardoBruchez <bruchezleonardo@gmail.com>
2026-05-15 17:38:48 -0300

    fix: ajusta rota base do vite para github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Supondo que o nome do seu repositório seja "bruchezDev.guia"
 export default defineConfig({
   plugins: [react()],
-  base: "/bruchezDev.guia/", // IMPORTANTE: O nome do repo com as duas barras!
+  base: "/bruchezdev-guia/",
 });
