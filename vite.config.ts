d1dd2b36213a3e21672b0574a2481e640fd6beed
LeonardoBruchez <bruchezleonardo@gmail.com>
2026-05-15 17:34:42 -0300

    update: Changes to the project configuration for GitHub.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Supondo que o nome do seu repositório seja "bruchezDev.guia"
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/bruchezDev.guia/", // IMPORTANTE: O nome do repo com as duas barras!
+});
